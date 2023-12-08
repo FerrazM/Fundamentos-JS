03fba0e078947ddd9e9ebe4c41bd6b9995267165
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-12-08 16:27:34 -0300

    O JavaScript e o Nodejs

--- console.js
+++ console.js
@@ -1,11 +1,20 @@
-// log -> registro
+// // log -> registro
 
-const minhaVar = true;
+// const minhaVar = true;
 
-// console.log(245)
-// console.log("eu sou um texto")
-// console.log(minhaVar)
+// // console.log(245)
+// // console.log("eu sou um texto")
+// // console.log(minhaVar)
 
-// tratamento de erro!
+// // tratamento de erro!
 
-console.error('deu erro!')
+// console.error('deu erro!')
+
+// mbora seja o mais utilizado, .log() é um dos vários métodos que podemos utilizar para exibir informações na chamada “saída padrão” - o terminal - enquanto estamos desenvolvendo uma aplicação. A palavra log significa algo como “registro”, então este método apenas registra no terminal o que passamos entre os parênteses, por exemplo o conteúdo de uma variável ou o resultado de uma operação.
+
+// Entre os outros métodos, existem:
+
+// console.error() para exibir mensagens de erro;
+// console.table() para visualizar de forma mais organizada informações tabulares;
+// console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+// console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
---
+++ script.js
@@ -0,0 +1,19 @@
+// tipagem dinamica
+// untyped
+
+let minhaVar = 567;
+minhaVar = 'texto';
+minhaVar = true;
+
+// Multiparadigma: 
+//Conseguimos resolver o mesmo problema de várias maneiras diferentes
+
+// Oque é ES6 / ES2015:
+// É a versão do JS que saiu em 2015, e tambem a versão mais famosa, pois com ela veio a consolidação do JS
+
+// Linguagem interpretada:
+// Linguagem interpretada, nós escrevemos o nosso código e ele é executado quando é rodado.
+
+// Oque é NODEJS?
+// Node é o JAVASCRIPT, porém podemos usa-lo como interpretador no SERVIDOR no lugar do NAVEGADOR
+// Ou seja é uma ferramenta, que foi desenvolvida para conseguirmos utilizar o JS fora do navegador.
