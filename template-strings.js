674af87410765fc57710e0221c1d10af64da6c21
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-12-11 17:42:58 -0300

    Fuções

--- template-strings.js
+++ template-strings.js
@@ -1,5 +1,5 @@
-const nome = "Ju";
-const idade = 2021-1981;
+const nome = "Matheus";
+const idade = 2023-1997;
 const cidadeDeNascimento = "São Paulo";
 
 // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
@@ -7,5 +7,3 @@ const cidadeDeNascimento = "São Paulo";
 const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
 
 console.log(apresentacao)
-
-
